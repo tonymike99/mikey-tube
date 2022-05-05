33ec1aef15266b710b60198c116c84996efbe01f
Tony <tonymike99@gmail.com>
2022-05-05 22:56:15 +0530

    added indexes for components and pages

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export { Login } from "./Login/Login";
+export { Signup } from "./Signup/Signup";
+export { Home } from "./Home/Home";
+export { Videos } from "./Videos/Videos";
+export { History } from "./History/History";
+export { WatchLater } from "./WatchLater/WatchLater";
+export { Playlists } from "./Playlists/Playlists";
+export { PageNotFound } from "./PageNotFound/PageNotFound";
