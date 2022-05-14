32a8970f0f6a3c46947a850962d9e89dea9ec97b
Tony <tonymike99@gmail.com>
2022-05-14 18:36:10 +0530

    modified index for components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
 export { Footer } from "./Footer/Footer";
 export { Header } from "./Header/Header";
+export { Slideshow } from "./Slideshow/Slideshow";
+export { Aside } from "./Aside/Aside";
+export { CardCategory } from "./CardCategory";
+export { CardVideo } from "./CardVideo";
