5d4820134090dc0624e11c1cbefe790f959ec7e4
Tony <tonymike99@gmail.com>
2022-05-14 18:37:36 +0530

    modified index for context and reducer

--- src/hooks/context/index.js
+++ src/hooks/context/index.js
@@ -1,1 +1,5 @@
 export { ThemeProvider, useTheme } from "./theme-context";
+export {
+  VideoSectionsProvider,
+  useVideoSections,
+} from "./video-sections-context";
---
+++ src/hooks/reducer/index.js
@@ -0,0 +1,1 @@
+export { videoSectionsReducer } from "./video-sections-reducer";
