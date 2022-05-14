350f66709c80c885b5e3dad8ed41929200bc078c
Tony <tonymike99@gmail.com>
2022-05-14 18:44:17 +0530

    added Home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,28 @@
+import { Aside, CardCategory, Slideshow } from "../../components/index";
+import { useDocumentTitle } from "../../hooks/custom/index";
+import { categories } from "../../backend/db/categories";
+
 function Home() {
-  return null;
+  // SET DOCUMENT TITLE
+  useDocumentTitle("Home");
+
+  return (
+    <div className="main-container">
+      <Aside />
+
+      <main className="main">
+        <section>
+          <Slideshow />
+        </section>
+
+        <section className="videos">
+          {categories.map((category) => (
+            <CardCategory key={category._id} data={category} />
+          ))}
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export { Home };
