c7d383fc83a47cc3bc0687e2d7c0671756031dfb
Tony Mike Auxen <59578286+tonymike99@users.noreply.github.com>
2022-05-15 19:00:19 +0530

    Update Aside.jsx

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -2,51 +2,29 @@ import "./Aside.css";
 import { NavLink } from "react-router-dom";
 
 function Aside() {
+  const linkDetails = [
+    { to: "/", name: "HOME", icon: "fa-home" },
+    { to: "/explore", name: "EXPLORE", icon: "fa-compass" },
+    { to: "/history", name: "HISTORY", icon: "fa-history" },
+    { to: "/watch-later", name: "WATCH LATER", icon: "fa-clock" },
+    { to: "/playlists", name: "PLAYLISTS", icon: "fa-list-check" },
+    { to: "/liked-videos", name: "LIKED VIDEOS", icon: "fa-thumbs-up" },
+  ];
+
   return (
     <aside className="aside">
       <ul className="list list-stacked tabs">
-        <li>
-          <NavLink to="/">
-            <i className="fa-solid fa-home fa-lg"></i>
-            HOME
-          </NavLink>
-        </li>
-        <hr className="hr-thin" />
-        <li>
-          <NavLink to="/explore">
-            <i className="fa-solid fa-compass fa-lg"></i>
-            EXPLORE
-          </NavLink>
-        </li>
-        <hr className="hr-thin" />
-        <li>
-          <NavLink to="/history">
-            <i className="fa-solid fa-history fa-lg"></i>
-            HISTORY
-          </NavLink>
-        </li>
-        <hr className="hr-thin" />
-        <li>
-          <NavLink to="/watch-later">
-            <i className="fa-solid fa-clock fa-lg"></i>
-            WATCH LATER
-          </NavLink>
-        </li>
-        <hr className="hr-thin" />
-        <li>
-          <NavLink to="/playlists">
-            <i className="fa-solid fa-list-check fa-lg"></i>
-            PLAYLISTS
-          </NavLink>
-        </li>
-        <hr className="hr-thin" />
-        <li>
-          <NavLink to="/liked-videos">
-            <i className="fa-solid fa-thumbs-up fa-lg"></i>
-            LIKED VIDEOS
-          </NavLink>
-        </li>
-        <hr className="hr-thin" />
+        {linkDetails.map((linkDetail) => (
+          <>
+            <li>
+              <NavLink to={`${linkDetail.to}`}>
+                <i className={`fa-solid fa-home ${linkDetail.icon} fa-lg`}></i>
+                {`${linkDetail.name}`}
+              </NavLink>
+            </li>
+            <hr className="hr-thin" />
+          </>
+        ))}
       </ul>
     </aside>
   );
