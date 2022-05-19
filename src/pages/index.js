4c7a47c18681f0053f7905a3f088686888f9e606
Tony <tonymike99@gmail.com>
2022-05-19 19:21:25 +0530

    updated local indexes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
-export { Login } from "./Login/Login";
-export { Signup } from "./Signup/Signup";
+export { Auth } from "./Auth/Auth";
 export { Home } from "./Home/Home";
 export { Explore } from "./Explore/Explore";
 export { History } from "./History/History";
 export { WatchLater } from "./WatchLater/WatchLater";
 export { Playlists } from "./Playlists/Playlists";
 export { PageNotFound } from "./PageNotFound/PageNotFound";
-
