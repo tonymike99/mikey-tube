2c31467cad7f79a2bcdee420a7d4251713e78173
Tony <tonymike99@gmail.com>
2022-05-19 19:22:40 +0530

    updated App.js to handle private and restricted routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Footer, Header } from "./components/index";
+import { PrivateRoute, RestrictedRoute } from "./auth/index";
 import {
-  Login,
-  Signup,
+  Auth,
   Home,
   Explore,
   History,
@@ -19,13 +19,18 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/watch-later" element={<WatchLater />} />
-        <Route path="/playlists" element={<Playlists />} />
         <Route path="/*" element={<PageNotFound />} />
+
+        <Route element={<RestrictedRoute />}>
+          <Route path="/auth" element={<Auth />} />
+        </Route>
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/history" element={<History />} />
+          <Route path="/watch-later" element={<WatchLater />} />
+          <Route path="/playlists" element={<Playlists />} />
+        </Route>
       </Routes>
 
       <Footer />
