fcccabbd4d824cf16a4873bf280838adcb985cf7
Tony <tonymike99@gmail.com>
2022-05-19 19:19:44 +0530

    added auth context

---
+++ src/hooks/context/auth-context.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import { createContext, useContext, useState } from "react";
+
+const defaultObj = {};
+const AuthContext = createContext(defaultObj);
+
+const AuthProvider = ({ children }) => {
+  const [userDetails, setUserDetails] = useState(null);
+  const [encodedToken, setEncodedToken] = useState(null);
+
+  const authenticateLoginDetails = async (loginDetails) => {
+    try {
+      const params = {
+        method: "post",
+        url: "/api/auth/login",
+        data: loginDetails,
+      };
+
+      const loginResponse = await axios.request(params);
+
+      if (loginResponse.status === 200) {
+        setUserDetails(loginResponse.data.foundUser);
+        setEncodedToken(loginResponse.data.encodedToken);
+        localStorage.setItem("jwt-token", loginResponse.data.encodedToken);
+      }
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
+  const authenticateSignupDetails = async (signupDetails) => {
+    try {
+      const params = {
+        method: "post",
+        url: "/api/auth/signup",
+        data: signupDetails,
+      };
+
+      const signupResponse = await axios.request(params);
+
+      if (signupResponse.status === 201) {
+        setUserDetails(signupResponse.data.createdUser);
+        setEncodedToken(signupResponse.data.encodedToken);
+        localStorage.setItem("jwt-token", signupResponse.data.encodedToken);
+      }
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
+  const logoutUserDetails = () => {
+    setUserDetails(null);
+    setEncodedToken(null);
+    localStorage.removeItem("jwt-token");
+  };
+
+  const valueObj = {
+    userDetails,
+    encodedToken,
+    authenticateLoginDetails,
+    authenticateSignupDetails,
+    logoutUserDetails,
+  };
+
+  // ****************************************************************************************************
+
+  return (
+    <AuthContext.Provider value={valueObj}>{children}</AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { AuthProvider, useAuth };
