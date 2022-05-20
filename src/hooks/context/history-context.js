8ccec480722ef332b0f7252dd1c6938dce28539c
Tony <tonymike99@gmail.com>
2022-05-20 08:55:29 +0530

    added history context

---
+++ src/hooks/context/history-context.js
@@ -0,0 +1,115 @@
+import axios from "axios";
+import { createContext, useContext, useState, useEffect } from "react";
+import { useAuth } from "./index";
+
+const defaultObj = {};
+const HistoryContext = createContext(defaultObj);
+
+const HistoryProvider = ({ children }) => {
+  const { encodedToken } = useAuth();
+  const [historyVideos, setHistoryVideos] = useState([]);
+
+  useEffect(() => {
+    if (encodedToken) {
+      (async () => {
+        try {
+          const params = {
+            method: "get",
+            url: "/api/user/history",
+            headers: {
+              authorization: encodedToken,
+            },
+          };
+
+          const historyVideosResponse = await axios.request(params);
+
+          if (historyVideosResponse.status === 200) {
+            setHistoryVideos(historyVideosResponse.data.history);
+          }
+        } catch (error) {
+          console.log(error.response.data);
+        }
+      })();
+    }
+  }, [encodedToken]);
+
+  const addVideoToHistory = async (video) => {
+    try {
+      const params = {
+        method: "post",
+        url: "/api/user/history",
+        data: { video },
+        headers: {
+          authorization: encodedToken,
+        },
+      };
+
+      const historyVideosResponse = await axios.request(params);
+
+      if (historyVideosResponse.status === 201) {
+        setHistoryVideos(historyVideosResponse.data.history);
+      }
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
+  const removeVideoFromHistory = async (videoId) => {
+    try {
+      const params = {
+        method: "delete",
+        url: `/api/user/history/${videoId}`,
+        headers: {
+          authorization: encodedToken,
+        },
+      };
+
+      const historyVideosResponse = await axios.request(params);
+
+      if (historyVideosResponse.status === 200) {
+        setHistoryVideos(historyVideosResponse.data.history);
+      }
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
+  const removeAllVideosFromHistory = async () => {
+    try {
+      const params = {
+        method: "delete",
+        url: "/api/user/history/all",
+        headers: {
+          authorization: encodedToken,
+        },
+      };
+
+      const historyVideosResponse = await axios.request(params);
+
+      if (historyVideosResponse.status === 200) {
+        setHistoryVideos(historyVideosResponse.data.history);
+      }
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
+  const valueObj = {
+    historyVideos,
+    addVideoToHistory,
+    removeVideoFromHistory,
+    removeAllVideosFromHistory,
+  };
+
+  // ****************************************************************************************************
+
+  return (
+    <HistoryContext.Provider value={valueObj}>
+      {children}
+    </HistoryContext.Provider>
+  );
+};
+
+const useHistory = () => useContext(HistoryContext);
+
+export { HistoryProvider, useHistory };
