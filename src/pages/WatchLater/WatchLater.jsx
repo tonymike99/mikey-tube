dfb791ed3124cb129a16a833554349c63d9be69f
Tony <tonymike99@gmail.com>
2022-05-20 08:53:43 +0530

    added watch later page

--- src/pages/WatchLater/WatchLater.jsx
+++ src/pages/WatchLater/WatchLater.jsx
@@ -1,5 +1,32 @@
+import { Aside, CardVideo } from "../../components/index";
+import { useDocumentTitle } from "../../hooks/custom/index";
+import { useWatchLater } from "../../hooks/context/index";
+
 function WatchLater() {
-  return null;
+  // SET DOCUMENT TITLE
+  useDocumentTitle("Watch Later");
+
+  const { watchLaterVideos } = useWatchLater();
+
+  // ****************************************************************************************************
+
+  return (
+    <div className="main-container">
+      <Aside />
+
+      <main className="main">
+        <section className="videos">
+          {watchLaterVideos.length ? (
+            watchLaterVideos.map((watchLaterVideo) => (
+              <CardVideo key={watchLaterVideo._id} video={watchLaterVideo} />
+            ))
+          ) : (
+            <h1>Watch Later is empty!</h1>
+          )}
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export { WatchLater };
