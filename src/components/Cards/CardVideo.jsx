2b687498f80e61c32bcb631681a04bbd3088f2ab
Tony <tonymike99@gmail.com>
2022-05-22 11:13:21 +0530

    added logic to handle add/remove to/from playlist

--- src/components/Cards/CardVideo.jsx
+++ src/components/Cards/CardVideo.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
+import { useDocumentTitle } from "../../hooks/custom/index";
 import {
   useAuth,
   useHistory,
   useWatchLater,
   useLiked,
+  usePlaylist,
+  useVideosAndCategories,
 } from "../../hooks/context/index";
 
 function CardVideo({ video }) {
@@ -14,8 +18,21 @@ function CardVideo({ video }) {
   const { watchLaterVideos, addVideoToWatchLater, removeVideoFromWatchLater } =
     useWatchLater();
   const { likedVideos, addVideoToLiked, removeVideoFromLiked } = useLiked();
+  const {
+    playlists,
+    getPlaylists,
+    addVideoToPlaylist,
+    removeVideoFromPlaylist,
+  } = usePlaylist();
+  const { getVideo } = useVideosAndCategories();
+  const [isModal, setIsModal] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
+
+  // ****************************************************************************************************
+
+  // SET DOCUMENT TITLE
+  useDocumentTitle(`${title}`);
 
   // ****************************************************************************************************
 
@@ -37,17 +54,26 @@ function CardVideo({ video }) {
     ? true
     : false;
 
+  const isVideoInPlaylist = (singlePlaylistId) => {
+    let playlist = playlists.find(
+      (playlist) => playlist._id === singlePlaylistId
+    );
+    if (playlist !== -1)
+      return playlist.videos?.find((video) => video._id === _id) ? true : false;
+    return false;
+  };
+
   // ****************************************************************************************************
 
-  const handleAddVideoToHistoryOnClick = () => {
+  const handleGetVideoAndAddVideoToHistoryButtonOnClick = () => {
+    getVideo(_id);
+
     if (encodedToken && !IsVideoInHistory) {
       addVideoToHistory(video);
-    } else {
-      navigate("/auth");
     }
   };
 
-  const handleRemoveVideoFromHistoryOnClick = () => {
+  const handleRemoveVideoFromHistoryButtonOnClick = () => {
     if (encodedToken && IsVideoInHistory) {
       removeVideoFromHistory(_id);
     } else {
@@ -73,6 +99,25 @@ function CardVideo({ video }) {
     }
   };
 
+  const handlePlaylistsOpenButtonOnClick = () => {
+    if (encodedToken) {
+      setIsModal(!isModal);
+    } else {
+      navigate("/auth");
+    }
+  };
+
+  const handlePlaylistsCloseButtonOnClick = () => {
+    setIsModal(!isModal);
+  };
+
+  const handleEachPlaylistButtonOnChange = (e, playlistId) => {
+    getPlaylists();
+    e.target.checked
+      ? addVideoToPlaylist(playlistId, video)
+      : removeVideoFromPlaylist(playlistId, video._id);
+  };
+
   // ****************************************************************************************************
 
   return (
@@ -82,7 +127,7 @@ function CardVideo({ video }) {
           className="image-responsive"
           src={thumbnailUrl}
           alt={title}
-          onClick={handleAddVideoToHistoryOnClick}
+          onClick={handleGetVideoAndAddVideoToHistoryButtonOnClick}
         />
       </Link>
 
@@ -108,17 +153,60 @@ function CardVideo({ video }) {
           )}
         </span>
 
-        <span className="pointer" onClick={handleRemoveVideoFromHistoryOnClick}>
+        <span
+          className="pointer"
+          onClick={handleRemoveVideoFromHistoryButtonOnClick}
+        >
           {location.pathname === "/history" && IsVideoInHistory && (
             <i className="fa-solid fa-trash"></i>
           )}
         </span>
+
+        <span className=" pointer" onClick={handlePlaylistsOpenButtonOnClick}>
+          <i className="fa-solid fa-list-check"></i>
+        </span>
+
+        {isModal && (
+          <div className="modal-window">
+            <div>
+              <div className="margin-2">
+                <h1 className="text-center">Playlists</h1>
+                <span
+                  className="modal-close"
+                  onClick={handlePlaylistsCloseButtonOnClick}
+                >
+                  <i className="fa-solid fa-xmark"></i>
+                </span>
+              </div>
+
+              <div className="playlists-list">
+                {playlists.map((singlePlaylist) => (
+                  <div key={singlePlaylist._id}>
+                    <input
+                      type="checkbox"
+                      name={singlePlaylist.title}
+                      defaultValue={singlePlaylist.title}
+                      defaultChecked={isVideoInPlaylist(singlePlaylist._id)}
+                      onInput={(e) =>
+                        handleEachPlaylistButtonOnChange(e, singlePlaylist._id)
+                      }
+                    />
+                    <label htmlFor={singlePlaylist.title}>
+                      {" "}
+                      {singlePlaylist.title}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
 
       <Link to={`/explore/${_id}`}>
         <button
           className="btn btn-secondary btn-width-100 pointer"
-          onClick={handleAddVideoToHistoryOnClick}
+          onClick={handleGetVideoAndAddVideoToHistoryButtonOnClick}
         >
           Watch Now
         </button>
