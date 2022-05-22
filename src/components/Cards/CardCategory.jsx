aebf245001eca6bcd540ae3a87a94a1c676ebfc3
Tony <tonymike99@gmail.com>
2022-05-22 11:12:00 +0530

    modified CardCategory

--- src/components/Cards/CardCategory.jsx
+++ src/components/Cards/CardCategory.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 function CardCategory({ data }) {
   const { _id, name, imageUrl } = data;
 
+  // ****************************************************************************************************
+
   return (
     <Link key={_id} to="/explore">
       <div className="card relative round">
         <img
-          className="image-responsive round image-translucent card-category "
+          className="image-responsive round image-translucent card-category"
           src={imageUrl}
           alt={name}
         />
-        <div className="card-header absolute badge-center">
-          <h3 className="font-montserrat color-white text-center">
-            {name.toUpperCase()}
-          </h3>
+        <div className="card-header absolute badge-inside-center-stretch">
+          <h3 className="font-montserrat color-white">{name.toUpperCase()}</h3>
         </div>
       </div>
     </Link>
